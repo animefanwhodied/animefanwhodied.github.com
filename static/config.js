5c32c2daaaa961e8f42d9a39e528919f59198c2d
animefanwhodied <48730564+animefanwhodied@users.noreply.github.com>
2019-03-20 18:16:08 +0800

    Initial commit.

---
+++ static/config.js
@@ -0,0 +1,21 @@
+// ***********************************************************************************
+// ID of the Google spreadsheet
+// ***********************************************************************************
+// Make sure your spreadsheet is published (File > Publish to the web…)
+var spreadsheetId = "1r8CdjOK0wwYxUZokIvdhaQeUmBNzp0LvqOCiGjLDntg";
+
+// ***********************************************************************************
+// Your FC and IGN
+// ***********************************************************************************
+var friendCode = "2638-0798-7364";
+var inGameName = "AFWD";
+
+// ***********************************************************************************
+// URL of your Reddit/Twitter account
+// ***********************************************************************************
+var contactUrl = "http://reddit.com/u/AnimeFanWhoDied";
+
+// ***********************************************************************************
+// URL of your PGL trainer icon (optional)
+// ***********************************************************************************
+// Download it from the Pokémon Global Link and re-upload to an image-sharing website
